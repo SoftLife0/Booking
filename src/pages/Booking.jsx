a9c84e2da3838f309c0361c1db985ec209da1792
SoftLife0 <onikosiadewale18@gmail.com>
2024-03-21 13:15:24 +0000

    editing calender

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -7,8 +7,8 @@ import Calendar from 'react-calendar'
 function Booking() {
     const [date, setDate] = useState(new Date());
 
-    const onChange = () => {
-        setDate(date);
+    const onChange = (selectedDate) => {
+        setDate(selectedDate);
     }
 
   return (
@@ -17,8 +17,26 @@ function Booking() {
 
     <section>
         <div className='container'>
-            <div className='row defaultCard'>
-                <div className='col-md-6 offset-md-3 text-center'>
+            <div className='row'>
+                
+
+                <div className='col-md-12 custom-calendar-container'>
+                    <Calendar onChange={onChange} value={date} className="custom-calendar"/>
+                    {console.log(date)}
+                    {date.toDateString()}
+                </div>
+            </div>
+        </div>
+        </section>
+    </div>
+  )
+}
+
+
+export default Booking
+
+
+{/* <div className='col-md-6 offset-md-3 text-center'>
                     <img src={logo} alt='' style={{ width: '220px' }}/>
                     <p style={{textAlign:'center', width:'100%'}} >
                         <b>Kindly fill in the form to book a slot</b>
@@ -40,17 +58,4 @@ function Booking() {
                             <input type="time" className="inputCard" id="name" placeholder="Enter Phone Number"/>
                         </div>
                     </form>
-                </div>
-
-                <div className='col-md-6'>
-                <Calendar onChange={onChange} value={date} />
-                </div>
-            </div>
-        </div>
-        </section>
-    </div>
-  )
-}
-
-
-export default Booking
+                </div> */}
